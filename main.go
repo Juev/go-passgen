a46fc208ad1fad05da38400e2bbfa15acb3e3cdb
Denis Evsyukov <denis@evsyukov.org>
2018-01-07 17:27:43 +0300

    Added flags

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	length     = kingpin.Flag("length", "Password length").Short('l').Default("32").Int()
+	count      = kingpin.Flag("count", "Numbers of passwords").Short('c').Default("6").Int()
+	dictionary = kingpin.Flag("dictionary", "Dictionary what should be used on generation passwords").Short('d').String()
+	entropy    = kingpin.Flag("entropy", "Using entropy for generation passwords").Short('e').Bool()
 )
 
 func main() {
-	fmt.Println("Version: ", version, " BuildTime: ", buildTime, " Commit: ", commit)
+	kingpin.Version("Version: " + version + "\nBuildTime: " + buildTime + "\nCommit: " + commit)
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
 }
